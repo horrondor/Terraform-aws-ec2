24fbd9eaa1b90e1773879f825260d06bc7ec861f
Raju Bhattarai <rbhattarai525@gmail.com>
2026-01-24 16:55:40 +0545

    file[main.tf]: securtiy group name updated

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "instance1" {
   instance_type     = var.ec2_instance_type
   availability_zone = var.ec2_zone_name
   key_name          = var.ec2_key_name
-  security_group    = var.ec2_security_group_ids
+  security_groups    = var.ec2_security_group_ids
 
   tags = {
      Name = var.ec2_instance_name
