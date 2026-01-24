14206452ca3dac6e4907fb4eb8e1f8fe35986f07
Raju Bhattarai <rbhattarai525@gmail.com>
2026-01-24 17:45:26 +0545

    file[main.tf]: updated

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "instance1" {
 
-  ami               = var.ec2_ami_id
-  instance_type     = var.ec2_instance_type
-  availability_zone = var.ec2_zone_name
-  key_name          = var.ec2_key_name
-  security_groups    = var.ec2_security_group_ids
+  ami                    = var.ec2_ami_id
+  instance_type          = var.ec2_instance_type
+  availability_zone      = var.ec2_zone_name
+  key_name               = var.ec2_key_name
+  vpc_security_group_ids = var.ec2_security_group_ids
 
   tags = {
-     Name = var.ec2_instance_name
+    Name = var.ec2_instance_name
   }
 }
